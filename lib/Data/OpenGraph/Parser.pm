446378bd0f60675bb64a1a8abdd689ccf03234c9
Vaclav Haisman <vhaisman@gmail.com>
2017-11-13 23:54:40 +0100

    Rework parsing in terms of XPath queries.
    This allows to parse OpenGraph meta data in pages where the `og:type` meta
    comes after some other `og:` metas have been specified.

--- lib/Data/OpenGraph/Parser.pm
+++ lib/Data/OpenGraph/Parser.pm
@@ -1,71 +1,102 @@
 package Data::OpenGraph::Parser;
 use strict;
-use HTML::Parser;
 use Scalar::Util qw(reftype);
 
 
 sub new {
-    my $class = shift;
-    my $parser = HTML::Parser->new(
-        api_version => 3,
-        start_h => [ sub {
-            my ($self, $tag, $attr) = @_;
-
-            return unless $tag eq 'meta';
-
-            my $prop = $attr->{property};
-            my $content = $attr->{content};
-            return unless $prop && $content;
-            my $properties = $self->{properties};
-            my $basictype;
-            return unless ($prop =~ /^og:/
-                           || (exists $properties->{'type'}
-                               && ($basictype = $properties->{'_basictype'}) ne ''
-                               && $prop =~ /^(?:og:)?$basictype:.+$/));
-            $prop =~ s/^og://;
-            $basictype = $properties->{'_basictype'} if ! defined $basictype;
-            if (defined $basictype
-                && $basictype ne ''
-                && $prop =~ /^(?:og:)?$basictype:.+$/) {
-                if ($prop =~ /^$basictype:.+$/) {
-                    if (exists $properties->{$prop}) {
-                        if ((reftype($properties->{$prop}) // '') eq 'ARRAY') {
-                            push @{$properties->{$prop}}, $content;
-                        }
-                        else {
-                            $properties->{$prop} = [$properties->{$prop}, $content];
-                        }
-                    }
-                    else {
-                        $properties->{$prop} = $content;
-                    }
-                }
-            }
-            else {
-                if ($prop eq 'type') {
-                    if ($content =~ /^([^.]+)\..+$/) {
-                        $properties->{'_basictype'} = $1;
-                    } else {
-                        $properties->{'_basictype'} = $content;
-                    }
-                }
-                $properties->{$prop} = $content;
-            }
-        }, "self, tagname, attr" ],
-    );
-    return bless { parser => $parser }, $class;
+    my ($class, %args) = @_;
+    my $self = bless {
+	libxml => ($args{libxml} // 1) ? 1 : undef
+    }, $class;
 }
 
 sub parse_string {
     my ($self, $string) = @_;
 
-    my %properties;
-    my $parser = $self->{parser};
-    local $parser->{properties} = \%properties;
-    $parser->parse($string);
-    $parser->eof;
+    my $tree;
+    if ($self->{libxml}) {
+	use HTML::TreeBuilder::LibXML;
+	$tree = HTML::TreeBuilder::LibXML->new();
+    }
+    else {
+	use HTML::TreeBuilder::XPath;
+	$tree = HTML::TreeBuilder::XPath->new();
+    }
+
+    $tree->parse($string);
+    $tree->eof;
+
+    my $og_type = $tree->findnodes('//meta[@property="og:type" and @content]');
+    if (! defined($og_type)) {
+	$tree->delete;
+	return $self;
+    }
+
+    # There appears to be a slight difference in return value depending on
+    # whether HTML::TreeBuilder::LibXML or HTML::TreeBuilder::XPath is used.
+    if (reftype($og_type) eq 'HASH') {
+	$og_type = $og_type->get_nodelist->get_node(0);
+    }
+    elsif (reftype($og_type) eq 'ARRAY') {
+	$og_type = $og_type->[0];
+    }
+    else {
+	$tree->delete;
+	return $self;
+    }
+
+    my %properties = ();
+
+    my $content = $og_type->attr('content');
+    if ($content =~ /^([^.]+)\..+$/) {
+	$properties{'_basictype'} = $1;
+    } else {
+	$properties{'_basictype'} = $content;
+    }
+    my $basictype = $properties{'_basictype'};
+
+    my $metas = $tree->findnodes(
+	'//meta['
+	.'((starts-with(@property, "og:") and @property != "og:type")'
+	.' or starts-with(@property, "'.$basictype.':"))'
+	.'and @content'
+	.']');
+    for my $meta (@$metas) {
+	my $prop = $meta->attr('property');
+	$content = $meta->attr('content');
+	next unless $prop && $content;
+	$prop =~ s/^og://;
+	if ($prop =~ /^$basictype:.+$/) {
+	    if (exists $properties{$prop}) {
+		if ((reftype($properties{$prop}) // '') eq 'ARRAY') {
+		    push @{$properties{$prop}}, $content;
+		}
+		else {
+		    $properties{$prop} = [$properties{$prop}, $content];
+		}
+	    }
+	    else {
+		$properties{$prop} = $content;
+	    }
+	}
+	else {
+	    $properties{$prop} = $content;
+	}
+    }
+
+    $tree->delete;
 
     return \%properties;
 }
 
 1;
+
+
+# Local Variables:
+# mode: cperl
+# cperl-indent-parens-as-block: t
+# cperl-indent-level: 4
+# cperl-close-paren-offset: -4
+# cperl-continued-statement-offset: 0
+# cperl-tab-always-indent: t
+# End:
